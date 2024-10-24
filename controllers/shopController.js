ea70066df98f07ea2a7a349a63c5aca6f10103e2
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-10-25 01:09:12 +0700

    first commit

---
+++ controllers/shopController.js
@@ -0,0 +1,262 @@
+const { Shops, Products, Users } = require("../models");
+const { Op } = require("sequelize");
+
+const createShop = async (req, res) => {
+  const { name, adminEmail, userId } = req.body;
+
+  try {
+    const newShop = await Shops.create({
+      name,
+      adminEmail,
+      userId,
+    });
+
+    res.status(201).json({
+      status: "Success",
+      message: "Success create new Shop",
+      isSuccess: true,
+      data: {
+        newShop,
+      },
+    });
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Failed",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    } else if (error.name === "SequelizeDatabaseError") {
+      return res.status(400).json({
+        status: "Failed",
+        message: errorMessage || "Database Error",
+        isSuccess: false,
+        data: null,
+      });
+    } else {
+      res.status(500).json({
+        status: "Failed",
+        message: "An unexpected error occured",
+        isSuccess: false,
+        data: null,
+      });
+    }
+  }
+};
+
+const getAllShop = async (req, res) => {
+  try {
+    const { shopName, adminEmail, productName, stock } = req.query;
+
+    const conditions = {};
+    if (shopName) conditions.name = { [Op.iLike]: `%${shopName}%` };
+
+    const prodctCondition = {};
+    if (productName) prodctCondition.name = { [Op.iLike]: `%${productName}%` };
+    if (stock) prodctCondition.stock = stock;
+
+    const shops = await Shops.findAll({
+      include: [
+        {
+          model: Products,
+          as: "products",
+          attributes: ["name", "images", "stock", "price"],
+          where: prodctCondition,
+        },
+        {
+          model: Users,
+          as: "user",
+          attributes: ["name"],
+        },
+      ],
+      attributes: ["name", "adminEmail"],
+      where: conditions,
+    });
+
+    const totalData = shops.length;
+
+    res.status(200).json({
+      status: "Success",
+      message: "Success get shops data",
+      isSuccess: true,
+      data: {
+        totalData,
+        shops,
+      },
+    });
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Fail",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    res.status(500).json({
+      status: "Fail",
+      message: error.message,
+      isSuccess: false,
+      data: null,
+    });
+  }
+};
+const getShopById = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const Shop = await Shops.findOne({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).json({
+      status: "Success",
+      message: "Success get shop data",
+      isSuccess: true,
+      data: {
+        Shop,
+      },
+    });
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Fail",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    res.status(500).json({
+      status: "Fail",
+      message: error.message,
+      isSuccess: false,
+      data: null,
+    });
+  }
+};
+
+const updateShop = async (req, res) => {
+  const id = req.params.id;
+  const { name, adminEmail } = req.body;
+
+  try {
+    const Shop = await Shops.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!Shop) {
+      res.status(404).json({
+        status: "Fail",
+        message: "Data not found",
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    await Shops.update({
+      name,
+      adminEmail,
+    });
+
+    res.status(200).json({
+      status: "Success",
+      message: "Success update shop",
+      isSuccess: true,
+      data: {
+        Shop: {
+          id,
+          name,
+          stock,
+          price,
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Fail",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    res.status(500).json({
+      status: "Fail",
+      message: error.message,
+      isSuccess: false,
+      data: null,
+    });
+  }
+};
+
+const deleteShop = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const Shop = await Shops.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!Shop) {
+      res.status(404).json({
+        status: "Fail",
+        message: "Data not found",
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    await Shops.destroy();
+
+    res.status(200).json({
+      status: "Success",
+      message: "Success delete shop",
+      isSuccess: true,
+      data: null,
+    });
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Fail",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    res.status(500).json({
+      status: "Fail",
+      message: error.message,
+      isSuccess: false,
+      data: null,
+    });
+  }
+};
+
+module.exports = {
+  createShop,
+  getAllShop,
+  getShopById,
+  updateShop,
+  deleteShop,
+};
